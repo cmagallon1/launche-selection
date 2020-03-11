67e9d038e096cb52a15b1d5bf91f15f9030eb631
cmagallon1 <cesar9mv@gmail.com>
2020-03-11 16:43:23 -0600

    [add] users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :meals
+  resources :meals,:launch_packages, :users
   
-  resources :launch_packages
 end

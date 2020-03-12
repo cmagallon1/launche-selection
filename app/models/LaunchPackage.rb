78c58fbd6b3c0490fa844ac34b3ce767beadc66e
cmagallon1 <cesar9mv@gmail.com>
2020-03-12 09:49:34 -0600

    [ref] models

--- app/models/LaunchPackage.rb
+++
@@ -1,4 +0,0 @@
-class LaunchPackage < ApplicationRecord
-  belongs_to :user
-  belongs_to :meal
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+
+ validates :name, presence: true
+
  has_many :launch_packages
  has_many :meals, through: :launch_packages
 end 

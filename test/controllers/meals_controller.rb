9d9567971c1b311e30cc6cbc9990b221fa64890a
cmagallon1 <cesar9mv@gmail.com>
2020-03-12 10:03:37 -0600

    [fix] files

--- test/controllers/meals_controller.rb
+++ test/controllers/meals_controller.rb
@@ -2,14 +2,14 @@ require 'test_helper'
 
 class MealsControllerTest < ActionDispatch::IntegrationTest
   def test_redirect_to_new_meal
-    get new_meals_url
+    get new_meal_url
     assert_response :success
   end
 
   def test_create_new_meal
     assert_difference("Meal.count") do
-      post meals, params: { meal: attributes_for(:meal) }
-      assert_response :success
+      post meals_path, params: { meal: attributes_for(:meal) }
+      assert_redirected_to meals_path
     end
   end
 end

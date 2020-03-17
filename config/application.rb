6754e6b1854a58ea76b05251b29bfc1b705c8b3d
cmagallon1 <cesar9mv@gmail.com>
2020-03-17 14:47:00 -0600

    [ADD] environment configuration

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module LaunchSelection
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    config.serve_static_assets = true
     config.factory_bot.definition_file_paths = ["test/factories"]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :meals,:lunch_packages
   
+  if Rails.env.development?
+    get '/coverage', to: 'coverages#index'
+  end
+
   root 'meals#index'
 end

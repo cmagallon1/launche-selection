6545a9d47616d44d4d634201378906ae1fc51777
cmagallon1 <cesar9mv@gmail.com>
2020-03-18 03:11:22 -0600

    [ADD] config

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,8 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+#
+# Loads assets folder to covegare report, only visible for development
+if Rails.env.development?
+    Rails.application.config.assets.paths << Rails.root.join("public/coverage/assets")
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :meals,:lunch_packages, :sessions
+
+  if Rails.env.development?
+    get '/coverage', to: 'coverages#index'
+  end
+
+  root 'sessions#new'
 end

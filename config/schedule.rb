d8959b1b55e12a07d566829ca4d640b9108d5c39
cmagallon1 <cesar9mv@gmail.com>
2020-03-22 17:33:03 -0600

    [ADD] configurations

---
+++ config/schedule.rb
@@ -0,0 +1,20 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+set :environment, "development" 
+set :output, "log/cron_log.log"
+env :PATH, ENV['PATH']
+# Example:
+#
+# set :output, "/path/to/my/cron_log.log"
+#
+every '0 10 20 * *' do
+   rake "email:send_emails"
+ end
+#
+# every 4.days do
+#   runner "AnotherModel.prune_old_records"
+# end
+
+# Learn more: http://github.com/javan/whenever

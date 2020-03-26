0a78c9112112e9c66952e6434154a386dc9d7516
cmagallon1 <cesar9mv@gmail.com>
2020-03-25 20:19:30 -0600

    [REF] mailers

--- app/mailers/meal_mailer.rb
+++ app/mailers/meal_mailer.rb
@@ -1,9 +1,10 @@
 class MealMailer < ApplicationMailer
-  default from: 'cesar9mv@gmail.com'
+  default to: -> { User.all.pluck(:email) },
+    from: 'cesar9mv@gmail.com'
 
   def monthly_package
     @user = params[:user]
-    @link = "http://localhost:3000/users/#{@user.id}/lunch_packages/new"
+    @link = new_user_lunch_packages_url(@user.id)
     mail(to: @user.email ,subject: "Create your monthly package")
   end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,10 @@
 class UserMailer < ApplicationMailer
-  default from: 'cesar9mv@gmail.com'
+  default to: -> { User.where(role: 2).pluck(:email) },
+  from: 'cesar9mv@gmail.com'
 
   def new_package
     @user = params[:user]
-    @link = "http://localhost:3000/users/#{@user.id}/lunch_packages"
+    @link = user_lunch_packages_url(@user)
     mail(subject: "New package created by #{@user.name}")
   end
 end

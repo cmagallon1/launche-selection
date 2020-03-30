12ba68db21635e97994e71f282d11d81f9e8e91e
cmagallon1 <cesar9mv@gmail.com>
2020-03-30 12:40:48 -0600

    [ADD] sign in view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,27 @@
 class UsersController < ApplicationController
+  skip_before_action :authorized, only: [:new, :create]
+
   def index
     @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to meals_path
+    else
+      render 'new'
+    end  
+  end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :password, :email)
   end
 end

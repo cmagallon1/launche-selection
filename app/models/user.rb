fa30835ca53eca6155885c3cc66b47a82f6d7aa4
cmagallon1 <cesar9mv@gmail.com>
2020-03-30 12:41:17 -0600

    [ADD] user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :name, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP  }
 
   has_many :lunch_packages
   has_many :meals, through: :lunch_packages

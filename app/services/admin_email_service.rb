fef0173bc431c271bf366f9cd45b73a86d35473f
cmagallon1 <cesar9mv@gmail.com>
2020-03-30 22:35:06 -0600

    [REF] rename files and add new service

--- app/services/admin_email_service.rb
+++ app/services/admin_email_service.rb
@@ -1,12 +1,8 @@
-class EmailService 
+class AdminEmailService 
   class << self 
     def send_admins_email(user)
       email = UserMailer.with(user: user).new_package
       email.deliver_later
-    end
-
-    def send_monthly_email
-      User.all.each { |user| MealMailer.with(user: user).monthly_package.deliver_later }
     end
   end
 end
